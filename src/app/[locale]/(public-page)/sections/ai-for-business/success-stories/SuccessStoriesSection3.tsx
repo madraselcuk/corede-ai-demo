a3a1ea877e05e01a8eb61c234e303ed53330ea97
madraselcuk <selcukmadra@gmail.com>
2025-05-19 15:51:48 +0300

    .

--- src/app/[locale]/(public-page)/sections/ai-for-business/success-stories/SuccessStoriesSection3.tsx
+++ src/app/[locale]/(public-page)/sections/ai-for-business/success-stories/SuccessStoriesSection3.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import { useTranslations } from 'next-intl'
-import Image from 'next/image'
 import { motion } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
 
